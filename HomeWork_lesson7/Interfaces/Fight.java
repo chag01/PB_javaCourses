948688480380faf2bb5720e943ec0951c16f6e2e
chag01 <57625834+chag01@users.noreply.github.com>
2020-08-11 23:20:51 +0300

    Add files via upload

---
+++ HomeWork_lesson7/Interfaces/Fight.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public interface Fight {
+
+    void hit();
+
+}
---
+++ HomeWork_lesson7/Interfaces/Swim.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public interface Swim {
+
+    void swimStart ();
+    void swimStop ();
+}
